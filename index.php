f4dff643f5004b72a23d87b34e5df2c58fcd0500
k0nte <konte.u@gmail.com>
2023-10-31 01:40:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+﻿<?php
+if (isset($_GET["q"]))
+	require "redirect.php";
+?>
+<html style="font-family: Raleway, Verdana, Sans">
+<head>
+	<title>Bahnsuche</title>
+	<meta name="viewport" content="width=device-width, initial-scale=0.7, minimum-scale=0.7">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<style>
+		code { background: #eee; display: block; padding: 4px 10px; }
+		body > div { background: #fff3; margin: 30px auto; padding: 12px; width: 70%; }
+		a, a:visited { color: #23d}
+		#schmal { display: none }
+		@media (max-width:900px) { 
+			#schmal { display: block }
+			#breit  { display: none  }
+		}
+		span, table {
+			font-size: 1.2em;
+			margin: auto;
+		}
+	</style>
+</head>
+<body style="background: linear-gradient(177deg, rgba(198,106,126,1) 0%, rgba(212,160,198,1) 35%, rgba(255,207,192,1) 100%); width:100%;
+  margin: 0; display: flex; flex-flow: column; justify-content: center;text-align: center">
+	<div style="margin-top: 200px">
+		<div>
+			<h1>Bahn-Verbindungssuche</h1>
+		</div>
+		<form action="./">
+			<input id="input" name="q" style="width: 90%; max-width: 700px; margin: 10px; font-size: 1.2em; font-family: Cambria; padding: 9px" placeholder="Berlin Hamburg 19 [Uhr]" />
+			<script>document.getElementById("input").focus()</script>
+		</form>
+	</div>
+	<div>
+		<h1>Mögliche Eingaben</h1>
+		<table>
+			<tr>
+				<th>Von A nach B</th>
+				<td><code>Münster nach Frankfurt<br>Münster-Frankfurt<br>Münster Frankfurt</code></td>
+			</tr>
+			<tr>
+				<th>KFZ-Kennzeichen</th>
+				<td><code>ms f &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>= Münster nach Frankfurt</i></code></td>
+			</tr>
+			<tr>
+				<th>Zeitpunkt</th>
+				<td><code>Köln Bonn Dienstag  &nbsp;<i>= nächsten Dienstag</i><br>
+					Köln Bonn am 3.  &nbsp;&nbsp;&nbsp;&nbsp;<i>= z.B. am 3. Oktober</i><br>
+					Köln Bonn 3.10.  &nbsp;&nbsp;&nbsp;&nbsp;<i>= am 3. Oktober</i><br>
+					Köln Bonn 15:30  &nbsp;&nbsp;&nbsp;&nbsp;<i>= um 15:30 Uhr</i><br>
+					Köln Bonn 15h  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>oder 15</i><br>
+				</td>
+			</tr>
+			<tr>
+				<th>Einstellungen<!--<br>(werden für nächsten <br>Aufruf gespeichert)--></th>
+				<td><code>bahncard25&nbsp;&nbsp; <i>= mit Bahncard 25</i><br>
+					bc25 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>= mit Bahncard 25</i><br>
+					Nahverkehr &nbsp;&nbsp;<i>= nur Nahverkehr</i><br>
+					NAH    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>= nur Nahverkerh</i><br>
+					Klasse &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>= 1. Klasse</i></code>
+				</td>
+			</tr>
+			<tr>
+				<th>Beispiele</th>
+				<td><code>Rostock nach Hamburg Freitag 16h NAH<br>
+				B S 2. 15 bc <i>= Berlin nach Stuttgart am <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2. um 15 Uhr mit Bahncard25</i><br>
+				</code></td>
+			</tr>
+			<tr>
+				<th>Weitere Suchen</th>
+				<td><code>ICE 722 &nbsp;&nbsp;<i>= Zuginformationen</i><br>
+				Köln &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>= Bahnhofsinformationen</i><br>
+				k &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i>= Bahnhofsinformationen Köln</i></code>
+		</table>
+	</div>
+	<div>
+		<h1>Tipp</h1>
+		<span id="breit">Klicke Rechtsklick auf das Suchfeld und dann „Suchmaschine hinzufügen“ o.ä. – dadurch kannst du direkt über deinen Browser diese Suche benutzen.</span>
+		<span id="schmal">Tippe auf das Suchfeld und halte gedrückt – dort findest du eine Option, um diese Suche als Suchmaschine zu deinem Browser hinzuzufügen.</span>
+	</div>
+	<div>
+		<h1>Urheber</h1>
+		<a href="http://ummen.tk/">Konstantin Ummen</a>
+	</div>
+		
+</body>
+</html>
+<?php
+// DB Client Secret
+// 9cc94c471e26d163d680f4dae065b264
+?>
---
+++ redirect.php
@@ -0,0 +1,125 @@
+<?php
+date_default_timezone_set("Europe/Berlin");
+require "kennzeichen.php";
+$context = stream_context_create([
+		"http" => [
+			"header" => "Accept: application/vnd.de.db.ris+json
+DB-Client-Id: b8a97e9b2dd01a81302f7d2cc384599e
+DB-Api-Key: 9cc94c471e26d163d680f4dae065b264"
+		] ]);
+	
+$q = urldecode($_GET["q"]);
+preg_match("#([\pL\/]+)( *\-| nach| n| ) *([\pL\/]+)( *(\d{1,2})\.((\d{1,2})(\.)?)?| (am )?(mo|di|mi|do|fr|sa|so)\pL*?)?( *(\d{1,2})([,.:h ](\d{1,2}))?)?#ui", $q, $match);
+$start  = trim(@$match[1]);
+$ziel   = trim(@$match[3]);
+if (!$ziel) {
+	preg_match("#^([\pL\/]+) (\pL+)#ui", $q, $match2);
+	$start = $match2[1];
+	$ziel  = $match2[2];
+}
+$stunde = @$match[12] ? @$match[12] : date("H");
+$tag	= @$match[5]  ? @$match[5]  : ($stunde < date("H") ? date("d")+1 : date("d"));
+$monat  = @$match[7]  ? @$match[7]  : ($tag < date("d") ? date("m")+1 : date("m"));
+$wotag  = @$match[10];
+$minute = @$match[14] ? @$match[14] : (@$match[12] ? 0 : date("i"));
+
+$bc25 = preg_match("#(bc *25|bahn *card *25|bc$)#ui", $q);
+$klasse1 = preg_match("#(klasse|class)#ui", $q);
+$nahverkehr = preg_match("#(NAH|(?i)nahverkehr)#", $q);
+
+if (strlen($start) <= 3)
+	$start = isset($kennzeichen[mb_strtoupper($start)]) ? $kennzeichen[mb_strtoupper($start)] : $start;
+if (strlen($ziel) <= 3)
+	$ziel = isset($kennzeichen[mb_strtoupper($ziel)]) ? $kennzeichen[mb_strtoupper($ziel)] : $ziel;
+
+if ($ziel) {		// Fahrplanauskunft
+	function myUrlEncode($string) {
+		$entities = array('%28', '%29', "+");
+		$replacements = array("(", ")", "%20");
+		return str_replace($entities, $replacements, urlencode($string));
+	}
+
+	$file = file_get_contents("https://apis.deutschebahn.com/db-api-marketplace/apis/ris-stations/v1/stop-places/by-name/".urlencode($start), false, $context);
+	$station = json_decode($file)->stopPlaces;
+	$start = $station[0]->names->DE->nameLong;
+	
+	
+	$file = file_get_contents("https://apis.deutschebahn.com/db-api-marketplace/apis/ris-stations/v1/stop-places/by-name/".urlencode($ziel), false, $context);
+	$station2 = json_decode($file)->stopPlaces;
+	$ziel = $station2[0]->names->DE->nameLong;
+	
+	$so = myUrlEncode($start);
+	$zo = myUrlEncode($ziel);
+	$soei = $station[0]->evaNumber;
+	$zoei = $station2[0]->evaNumber;
+	$kl = $klasse1 ? 1 : 2;
+	$r  = $bc25 ? "13:17:KLASSE_".($klasse1 ? 1 : 2).":1" : "13:16:KLASSENLOS:1"; //  			13:17:KLASSE_1:1			13:23:KLASSE_2:1		13:23:KLASSE_1:1
+	$long = str_replace(".", "", $station[0]->position->longitude);
+	$lat  = str_replace(".", "", $station[0]->position->latitude);
+	$soid = myUrlEncode("A=1@O=$start@X=$long@Y=$lat@U=80@L=$soei@B=1@p=1698259482@");
+	$long = str_replace(".", "", $station2[0]->position->longitude);
+	$lat  = str_replace(".", "", $station2[0]->position->latitude);
+	$zoid  = myUrlEncode("A=1@O=$ziel@X=$long@Y=$lat@U=80@L=$zoei@B=1@p=1698251742@");
+	$sotzot = "sot=ST&zot=ST";
+	
+	if ($wotag) {
+		$replace = ["mo" => "Monday", "di" => "Tuesday", "mi" => "Wednesday", "do" => "Thursday", "fr" => "Friday", "sa" => "Saturday", "so" => "Sunday"];
+		$date = strtotime("next ".$replace[strtolower($wotag)])+$stunde*60*60+$minute*60;
+	} else {
+		echo "hi<br>";
+		$date = mktime($stunde, $minute, 0, $monat, $tag);
+		// if ($date < time())
+			// $date = mktime($stunde, $minute, 0, $monat+1, $tag);
+	}
+	
+	$hd = date("Y-m-d\TH:i:s", $date);
+	$rest = "hza=D&ar=false&s=true&d=false&hz=%5B%5D&fm=false&bp=false";
+	if ($nahverkehr)
+		$rest .= "&vm=03,04,05,06,07,08,09";
+	$hash = "sts=true&so=$so&zo=$zo&kl=$kl&r=$r&soid=$soid&zoid=$zoid&$sotzot&soei=$soei&zoei=$zoei&hd=$hd&$rest";
+	$url = "https://www.bahn.de/buchung/fahrplan/suche#$hash";
+	
+	header("Location: $url");
+	exit;
+} else if (preg_match("#\d{2,}#", $q)) {		// Zug
+	$postdata = http_build_query(
+		array(		'lang' => 'de',
+					'zugnr' => $q			)		);
+	$context = stream_context_create(array('http' =>
+		array(
+			'method'  => 'POST',
+			'header'  => 'Content-Type: application/x-www-form-urlencoded
+sec-ch-ua: "Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"
+sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Windows"
+origin: https://www.zugfinder.net
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36
+accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+sec-fetch-site: same-origin
+sec-fetch-mode: navigate
+accept-language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7,el;q=0.6,fr;q=0.5,es;q=0.4,lb;q=0.3',
+			'content' => $postdata
+		)
+	));
+	$result = get_headers('https://www.zugfinder.net/search.php', true, $context);
+	header("Location: ".$result["Location"]);
+	exit;
+	// var_dump($result);
+} else {										// Bahnhof
+	$file = file_get_contents("https://apis.deutschebahn.com/db-api-marketplace/apis/ris-stations/v1/stop-places/by-name/".urlencode($q), false, $context);
+	$station = json_decode($file)->stopPlaces;
+	$start = $station[0]->names->DE->nameLong;
+	
+	$bahnhof = strtolower(str_replace(["ß", "ä", "ö", "ü", "(", ")", " "], ["ss", "ae", "oe", "ue", "-", "-", "-"], $start));
+	header("Location: https://www.bahnhof.de/$bahnhof");
+	exit;
+}
+	// echo "<pre>";
+	//print_r($station2[0]);
+	//echo "https://www.bahn.de/buchung/fahrplan/suche#$hash";
+	//print_r($match);
+	// echo "</pre>";
+	//var_dump($match);
+	echo "<br><br>Start: $start<br>Ziel: $ziel<br>Tag: $tag<br>Wochentag: $wotag<br>Monat: $monat<br>Stunde: $stunde<br>Minute: $minute<br>hd: $hd - $date - ".time();
+	// exit("<script>location.hash = ''</script>");
+	?>
