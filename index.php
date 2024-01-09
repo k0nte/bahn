e3fb8996bc8445fe36130012ead5988e8753d577
k0nte <konte.u@gmail.com>
2024-01-09 13:56:49 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 ﻿<?php
 $nachricht = "";
+			$heute = mktime(0,0,0);
 if (isset($_GET["xq"]))
 	require "redirect.php";
 ?>
@@ -8,69 +9,7 @@ if (isset($_GET["xq"]))
 	<title>Bahnsuche</title>
 	<meta name="viewport" content="width=device-width, initial-scale=0.75, minimum-scale=0.75">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<style>
-		code { 
-			background: #eee; 
-			display: block; 
-			padding: 4px 10px; 
-			white-space: pre-wrap;
-			max-width: calc(95vw - 42px);
-			margin: auto;
-		}
-		body > div, footer, #opts > p { 
-			background: #fff3; 
-			margin: 30px auto; 
-			padding: 12px; width: 70%; 
-			font-size: 1.1em;
-		}
-		footer a, footer a:visited {	color: #23d }
-		a:hover {						color: #34e }
-		#breit, #schmal { padding: 10px 50px; display: block }
-		#schmal { display: none }
-		@media (max-width:900px) { 
-			#schmal { display: block }
-			#breit  { display: none  }
-		}
-		span, #flex, a {
-			font-size: 1.2em;
-			margin: auto;
-		}
-		#flex {
-			display: flex;
-			width: min(95%, 800px);
-			flex-wrap: wrap;
-			align-items: center;
-			justify-content: center;
-			gap: 10px;
-		}
-		#flex > div {
-			min-width: 700px;
-		}
-		#flex > div:nth-child(2n+1) {
-			flex-basis: 30%;
-			font-weight: bold;
-			min-width: 200px;
-		}
-		#flex > div:nth-child(2n) {
-			flex-basis: 60%;
-			text-align: left;
-			min-width: 23em;
-		}
-		.opt {
-			border-radius: 5px;
-			border: 1px solid #95c;
-			background: #e9d5f5;
-			margin: 4px;
-			padding: 4px 8px;
-			display: inline-block;
-			color: #000;
-			text-decoration: none;
-		}
-		#opts > div {
-		  text-align: left;
-		  column-count: 2;
-		}
-	</style>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body style="background: linear-gradient(177deg, rgba(198,106,126,1) 0%, rgba(212,160,198,1) 35%, rgba(255,207,192,1) 100%); width:100%;
   margin: 0; display: flex; flex-flow: column; justify-content: center;text-align: center">
@@ -104,7 +43,7 @@ if (isset($_GET["xq"]))
 					inputField.click()
 					inputField.focus()
 				}, 100);
-				<?php if (isset($_COOKIE["PROMPT"])) { ?>
+				<?php if (isset($_COOKIE["PROMPT"]) AND !isset($_GET["xq"])) { ?>
 					setTimeout(function() {
 						answer = prompt("Wo soll die Fahrt hingehen?")
 						if (answer != null) 
@@ -131,28 +70,40 @@ if (isset($_GET["xq"]))
 				// Call the function to start displaying the placeholder text character by character
 				setPlaceholderText(0);
 			</script>
-		</form>
-		<?php 
-		foreach ($_COOKIE as $id => $cookie) {
-			if (in_array($id, ["station", "session"]))
-				continue;
-			echo "<a class='opt' href='?xq=-$id' title='Entfernen'>$id</a>";
-		}
-		echo '<div style="display:inline-block;width:30px"></div>';
-		if (isset($_COOKIE["station"]))
-			foreach ($_COOKIE["station"] as $var => $station) {
-				echo "<a class='opt' href='?xq=-$var' title='Entfernen'>$var: $station</a>";
+			<?php 
+			$found = false;
+			foreach ($_COOKIE as $id => $cookie) {
+				if (in_array($id, ["station", "session"]))
+					continue;
+				if (!$found) {
+					echo '<div style="clear:both"></div>';
+					$found = true;
+				}
+				echo "<a class='opt' href='?xq=-$id' title='Entfernen'>$id</a>";
 			}
-		?>
+			if (isset($_COOKIE["station"])) {
+				if ($found)
+					echo '<div style="display:inline-block;width:30px"></div>';
+				else 
+					echo '<div style="clear:both"></div>';
+				foreach ($_COOKIE["station"] as $var => $station) {
+					echo "<a class='opt' href='?xq=-$var' title='Entfernen'>$var: $station</a>";
+				}
+			}
+			?>
 		<div style="display:inline-block;width:30px"></div>
 		<a class='opt' href="#" onclick="document.getElementById('opts').hidden = !document.getElementById('opts').hidden; return false">⚙</a>
+		</form>
 		<div id="opts">
 			<p>Wähle Einstellungen, die für alle zukünftigen Suchen gespeichert werden. Willst du bei einer Suche die Einstellung ignorieren, schreibe „-Einstellung“, also z.B. „-nah“.</p>
 			<div>
 			<?php 
-				$opts = ["BC25" => "Bahncard 25", "NAH" => "Nur Nahverkehr", "BEST" => "Bestpreise", "RAD" => "Mit Fahrrad", "LANG" => "Auch langsame Verbindungen", "KLASSE" => "1. Klasse", "BPFERN" => "Bestpreise ab 120km außer bei NAH", "PROMPT" => "Zeige die Tastatur bei Mobilgeräten sofort"];
+				$opts = ["BC25|BC50" => "Bahncard", "BEST|BPFERN" => "Bestpreise (BPFERN: ab 120km außer bei NAH)", "NAH" => "Nur Nahverkehr", "RAD" => "Mit Fahrrad", "LANG" => "Auch langsame Verbindungen", "KLASSE" => "1. Klasse", "DIREKT" => "Nur Direktverbindungen", "PROMPT" => "Zeige die Tastatur bei Mobilgeräten sofort"];
 				foreach ($opts as $key => $desc) {
-					echo "<a class='opt' href='?xq=$key'>$key</a> $desc<br>";
+					$keys = explode("|", $key);
+					foreach ($keys as $k)
+						echo "<a class='opt' href='?xq=$k'>$k</a>";
+					echo " $desc<br>";
 				}
 			?>
 			</div>
@@ -180,7 +131,7 @@ Münster Nord Frankfurt Süd</b></code></div>
 			</div>
 			<div>Einstellungen<!--<br>(werden für nächsten <br>Aufruf gespeichert)--></div>
 			<div>
-				<code><b>Bahncard25</b> / <b>bc</b>     <i>= mit Bahncard 25</i>
+				<code><b>Bahncard25</b> / <b>bc25</b>     <i>= mit Bahncard 25</i>
 <b>Nahverkehr</b> / <b>nah</b>    <i>= nur Nahverkehr</i>
 <b>Bestpreise</b> / <b>best</b>   <i>= Bestpreise anzeigen</i>
 <b>Fahrrad</b>    / <b>rad</b>    <i>= mit Fahrrad</i>
@@ -189,7 +140,7 @@ Münster Nord Frankfurt Süd</b></code></div>
 			</div>
 			<div>Beispiele</div>
 			<div><code><b>Rostock nach Hamburg Freitag 16h Nah</b>  <i>= nächsten Freitag um 16 Uhr im Nahverkehr</i>
-<b>B S 2. 15 bc</b> <i>= Berlin nach Stuttgart am 2. um 15 Uhr mit Bahncard25</i></code>
+<b>B S 2. 15 bc25</b> <i>= Berlin nach Stuttgart am 2. um 15 Uhr mit Bahncard25</i></code>
 			</div>
 			<div>Bahnhöfe speichern</div>
 			<div><code><b>var = Baunatal Guntershausen</b>
@@ -198,7 +149,8 @@ Münster Nord Frankfurt Süd</b></code></div>
 			<div>Weitere Suchen</div>
 			<div><code><b>ICE 722</b>      <i>= Zuginformationen</i>
 <b>Köln</b> / <b>k</b>     <i>= Bahnhofsinformationen</i>
-<b>A B Kalender</b> <i>= Bestpreiskalender</i></code>
+<b>A B Kalender</b> <i>= Bestpreiskalender</i>
+<b><a href="https://travic.app/">Karte</a></b>        <i>= Zugverfolgung</i></code>
 			</div>
 		</div>
 	</div>
@@ -206,12 +158,25 @@ Münster Nord Frankfurt Süd</b></code></div>
 		<h1>Tipps</h1>
 		<p><span id="breit" title="URL: https://bahn.ummen.tk/?xq=%s">Klicke Rechtsklick auf das Suchfeld und dann „Suchmaschine hinzufügen“ o.ä. – dadurch kannst du direkt über deinen Browser diese Suche benutzen.</span>
 		<span id="schmal">Tippe auf das Suchfeld und halte gedrückt – dort findest du eine Option, um diese Suche als Suchmaschine zu deinem Browser hinzuzufügen.</span></p>
-		<p><span>Um herauszufinden, wie pünktlich ein Zug im letzten Monat war, kopiere die Zugkennung aus deinen Bahnergebnissen. Bei Regionalbahnen ist dies die lange Zahl unter Details, z.B. 10266 für RB 38.</span></p> 
+		<p><span>Um herauszufinden, wie pünktlich ein Zug im letzten Monat war, kopiere die Zugkennung aus deinen Bahnergebnissen in die Suche. Bei Regionalbahnen ist dies die lange Zahl unter Details, z.B. 10266 für RB 38.</span></p> 
 	</div>
 	<footer>
 		<h1>Urheber</h1>
-		<a href="http://ummen.tk/">Konstantin Ummen</a> | <a href="https://github.com/k0nte/bahn">Github</a><br>
-		<pre style="font-size: 1.1em">http://bahn.ummen.tk/?xq=[Suchwert]</pre>
+		<p>
+			<a href="http://ummen.tk/">Konstantin Ummen</a> | <a href="https://github.com/k0nte/bahn">Github</a><br>
+			Ich würde mich über eine kleine <a href="https://paypal.me/ummen">Spende über Paypal</a> freuen!
+		</p>
+		<div>
+		Anfragen heute: 
+			<?php
+			require_once "db_api.php";
+			$sql = $mysqli->query("SELECT * FROM bahn_stats WHERE day = $heute")->fetch_object();
+			if (!$sql)
+				echo "keine Nutzer";
+			else 
+				echo $sql->hits ." Anfragen, ".$sql->users." Nutzer";
+			?>			<pre style="font-size: 1.1em; margin:0">http://bahnu.de/?xq=[Suchwert]</pre>
+		</div>
 	</footer>
 		
 </body>
